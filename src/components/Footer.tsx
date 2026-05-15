53916c729953db711aa269b335ff796ae3703a5a
Mario Klau <128314759+marioklau@users.noreply.github.com>
2026-05-16 01:11:58 +0700

    Update image source path in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
               {/* Logo GMKI */}
               <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center overflow-hidden">
                 <img 
-                  src="/public/images/gmki_surabaya.png" 
+                  src="images/gmki_surabaya.png" 
                   alt="Logo GMKI" 
                   className="w-full h-full object-cover"
                 />
